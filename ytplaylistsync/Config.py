fab7e6d352b3404b61780211251c3b684dfe5ea8
MrXaxen <mrxaxen@gmail.com>
2022-03-24 21:57:21 +0100

    Drive api communication WIP, Auth

--- ytplaylistsync/Config.py
+++ ytplaylistsync/Config.py
@@ -3,5 +3,6 @@ from pathlib import Path
 resource_dir_path = Path("resources")
 
 resources = {
-    "auth_token": resource_dir_path / "token.json"
+    "auth_token" : resource_dir_path / "token.json",
+    "credentials" : resource_dir_path / "credentials.json",
 }
--- ytplaylistsync/ws/Authentication.py
+++ ytplaylistsync/ws/Authentication.py
@@ -11,10 +11,33 @@ class Authentication:
         self.__scopes = ['https://www.googleapis.com/auth/drive.metadata.readonly'] #TODO define the scope
         self.__creds = None
 
-    def authenticate(self) -> bool:
+    def authenticate(self) -> Credentials:
         if Config.resources['auth_token'].exists():
             self.__creds = Credentials.from_authorized_user_file(Config.resources['auth_token'], self.__scopes)
 
         if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
                 creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(Config.resources['credentials'], self.__scopes)
+                creds = flow.run_local_server(port=0)
+            with open(Config.resources['auth_token'], 'w') as token:
+                token.write(creds.to_json())
+        
+        return creds
+
+    def commWithApi(self, creds:Credentials): #TODO
+        try:
+            service = build('drive', 'v3', credentials=creds)
+
+            results = service.files.list(pageSize=10, fields="nextPageToken, files(id,name)").execute()
+            items = results.get('files', [])
+
+            if not items:
+                print('No files found.')
+                return
+            print('Files:')
+            for item in items:
+                print(f'{item["name"]} ({item["id"]})')
+        except HttpError as error:
+            print(f'An error occured while authenticating: {error}')
